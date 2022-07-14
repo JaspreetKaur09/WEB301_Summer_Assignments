057f48577e30a4272925c9d5d4a6dede57d85227
Jaspreet Kaur <91208959+JaspreetKaur09@users.noreply.github.com>
2022-07-14 14:17:44 -0700

    add the event handler.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 /*
-  Your name here
+  Jaspreet Kaur
+  0784780
 */
+
+$(function (){
+  function getCurrentLocation(){
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(success,showError);
+    } else {
+      $('#error').text("Geolocation is not supported on your browser!");
+    }
+  }
+});
