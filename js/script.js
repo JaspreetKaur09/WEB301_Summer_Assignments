dd72f334e00414aa741062c591d174c96b21591c
Jaspreet Kaur <91208959+JaspreetKaur09@users.noreply.github.com>
2022-07-14 14:26:40 -0700

    make a little mistake then change it

--- js/script.js
+++ js/script.js
@@ -3,30 +3,31 @@
   0784780
 */
 
-$(function (){
-  function getCurrentLocation(){
-    if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(success,showError);
+$(function () {
+  function getCurrentLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, showError);
     } else {
       $('#error').text("Geolocation is not supported on your browser!");
     }
-    function success(position){
+    function success(position) {
       $("#latitude").val(position.coords.latitude);
       $("#longitude").val(position.coords.longitude);
     }
-    function showError(error){
+    function showError(error) {
       $('#error').text("Location permission denied");
     }
-
-    $("#btnP").on("click", function () {
-      getCurrentLocation();
-    });
-    $("#btnSubmit").on("click", function () {
-      localStorage.setItem("firstname", $("#firstname").val());
-      localStorage.setItem("lastname", $("#lastname").val());
-      localStorage.setItem("longitude", $("#longitude").val());
-      localStorage.setItem("latitude", $("#latitude").val());
-      $('#success').text("Information submitted successfully");
-    });
   }
+
+  $("#btnP").on("click", function () {
+    getCurrentLocation();
+  });
+  $("#btnSubmit").on("click", function () {
+    localStorage.setItem("firstname", $("#firstname").val());
+    localStorage.setItem("lastname", $("#lastname").val());
+    localStorage.setItem("longitude", $("#longitude").val());
+    localStorage.setItem("latitude", $("#latitude").val());
+    $('#success').text("Information submitted successfully");
+  });
+
 });
